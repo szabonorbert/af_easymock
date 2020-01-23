cd3f2ec3fd66f3af040efe48bc4d401b9ddb8701
unknown <szabonorbert@detenyleg.com>
2020-01-23 10:28:40 +0100

    ready for mocking and testing

---
+++ service/src/test/java/hu/szabonorbert/af_easymock/service/CarStoreTest.java
@@ -0,0 +1,19 @@
+package hu.szabonorbert.af_easymock.service;
+import org.easymock.EasyMock;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CarStoreTest {
+
+    @Test
+    public void testFindAllBooks() {
+        System.out.println("OK");
+    }
+
+}
